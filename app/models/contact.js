6804c7d00785e752821f66aea6263424b99a7aad
saqib48 <saqibijaz488@gmail.com>
2025-06-18 00:03:31 +0500

    newone

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,29 +1,31 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const contactSchema = new Schema({
-    fullname: {
-        type: String,
-        required: [true, "Name is required"],
-        trim: true,
-        minlength: [2,"Name must be larger than 2 charachters"],
-        maxlength: [50,"Name must be lesser than 50 charachters"],
-    },
-    email:{
-        type:String,
-        required: [true, "Email is required"],
-        match: [/^[\w.%+_]+@[\w.-]+\.[A-Za-z]{2,}$/i, "Invalid email address"],
-
-    },
-    message:{
-         type:String,
-        required: [true, "Message is required"],
-    },
-    date:{
-        type: Date,
-        default: Date.now,
-    },
+  fullname: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+    minlength: [2, "Name must be larger than 2 characters"],
+    maxlength: [50, "Name must be lesser than 50 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    match: [
+      /^[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
+      "Invalid email address",
+    ],
+  },
+  message: {
+    type: String,
+    required: [true, "Message is required"],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
-const Contact = 
-mongoose.models.Contact || mongoose.model("Contact", contactSchema);
+
+const Contact = mongoose.models.Contact || mongoose.model("Contact", contactSchema);
 
 export default Contact;
